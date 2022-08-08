943a9bcab69fea21f329636efab932511f4017fe
freefly2601 <110852450+freefly2601@users.noreply.github.com>
2022-08-08 22:09:27 +0300

    Create names.py

---
+++ names.py
@@ -0,0 +1,12 @@
+# last names in the list
+authors = "Audre Lorde,Gabriela Mistral,Jean Toomer,An Qi,Walt Whitman,Shel Silverstein,Carmen Boullosa,Kamala Suraiyya,Langston Hughes,Adrienne Rich,Nikki Giovanni"
+
+author_names = authors.split(',')
+
+print(author_names)
+
+author_last_names = []
+for name in author_names:
+  author_last_names.append(name.split()[-1])
+  
+print(author_last_names)
